8d1cc9d4ec7e44b7e3ed7edf6b7ed115f9e1bda6
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-05-15 14:29:24 +0100

    Added top half of the project Added the all styles and elements required for the top half of the project(main.jsx)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,19 @@
+import './Main.css'
 export default function Main(){
     return(
-        <>
+        <main>
             <img src="src/assets/pfp.png"/>
             <h1>Laura Smith</h1>
             <h4>Fronted Developer</h4>
-            <p>laurasmith.website</p>
-            <button>
+            <p className='main--email'>laurasmith.website</p>
+            <div>
                 <img src='src/assets/Mail.svg'/>
-                Mail
-            </button>
-            <h2>About</h2>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-            <h2>Interest</h2>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
-        </>
+                <span>Mail</span>
+            </div>
+            <h3 className='main--about'>About</h3>
+            <p>I am a frontend developer with a particular interest<br/> in making things simple and automating daily<br/> tasks. I try to keep up with security and best<br/> practices, and am always looking for new things to learn.</p>
+            <h3>Interest</h3>
+            <p>Food expert. Music scholar. Reader. Internet<br/> fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </main>
     )
 }

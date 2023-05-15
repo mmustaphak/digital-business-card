8a515c7e2b955bfefb96ec1721acb0b8e93f9483
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-05-15 11:45:08 +0100

    Configured and added basic for main.jsx component

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+export default function Main(){
+    return(
+        <>
+            <img src="src/assets/pfp.png"/>
+            <h1>Laura Smith</h1>
+            <h4>Fronted Developer</h4>
+            <p>laurasmith.website</p>
+            <button>
+                <img src='src/assets/Mail.svg'/>
+                Mail
+            </button>
+            <h2>About</h2>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <h2>Interest</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </>
+    )
+}

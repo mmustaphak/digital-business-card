441f6c417775308371030b26dbfbc4559501c0ed
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-05-15 15:03:55 +0100

    Finished Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,11 @@ import "./Footer.css"
 export default function Footer(){
     return(
         <footer>
+            <img src="src/assets/Twitter Icon.svg" alt="" />
+            <img src="src/assets/Facebook Icon.svg" alt="" />
+            <img src="src/assets/Instagram Icon.svg" alt="" />
+            <img src="src/assets/Linkedin Icon.svg" alt="" />
+            <img src="src/assets/Github Icon.svg" alt="" />
         </footer>
     )
 }

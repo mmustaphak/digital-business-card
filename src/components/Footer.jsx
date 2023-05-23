3e9493556d640cb48f9b256af96e21bc8d87c3e8
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-05-23 16:14:12 +0530

    Fixed images not rendering on deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import "./Footer.css"
+import twitter from "../assets/Twitter Icon.svg"
+import facebook from "../assets/Facebook Icon.svg"
+import instagram from "../assets/Instagram Icon.svg"
+import linkedin from "../assets/Linkedin Icon.svg"
+import github from "../assets/GitHub Icon.svg"
+
 export default function Footer(){
     return(
         <footer>
-            <img src="./src/assets/Twitter Icon.svg" alt="" />
-            <img src="./src/assets/Facebook Icon.svg" alt="" />
-            <img src="./src/assets/Instagram Icon.svg" alt="" />
-            <img src="./src/assets/Linkedin Icon.svg" alt="" />
-            <img src="./src/assets/Github Icon.svg" alt="" />
+            <img src={twitter} alt="Twitter Icon" />
+            <img src={facebook} alt="Facebook Icon" />
+            <img src={instagram} alt="Instagram Icon" />
+            <img src={linkedin} alt="Linkedin Icon" />
+            <img src={github} alt="GitHub Icon" />
         </footer>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import './Header.css'
+import pfp from "../assets/pfp.png"
+import mail from "../assets/Mail.svg"
 export default function Header(){
     return(
         <header>
-            <img className='main--pfp' src="./src/assets/pfp.png"/>
+            <img className='main--pfp' src={pfp}/>
             <h1>Laura Smith</h1>
             <h4>Fronted Developer</h4>
             <p className='header--email'>laurasmith.website</p>
             <div>
-                <img src='./src/assets/Mail.svg'/>
+                <img src={mail}/>
                 <span>Email</span>
             </div>
         </header>
